08304538a75fa196eb10752c7d910e7e18ec6ad9
myozinoo1122 <132042459+myozinoo1122@users.noreply.github.com>
2023-07-18 16:49:49 +0630

    Add files via upload

---
+++ personal-portfolio/front/js/script.js
@@ -0,0 +1,13 @@
+window.onscroll = function () {
+  myFun();
+};
+var navbar = document.getElementById("navbar");
+var topPx = navbar.offsetTop;
+
+function myFun() {
+  if (window.pageYOffset >= topPx) {
+    navbar.classList.add("fixMe");
+  } else {
+    navbar.classList.remove("fixMe");
+  }
+}
